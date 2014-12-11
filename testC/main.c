f3f0c3041d629fbdec6c5378591ea549716ca07e
weichen <weichen@weichendeMacBook-Pro.local>
2014-12-12 03:43:11 +0800

    Initial Commit

---
+++ testC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  testC
+//
+//  Created by weichen on 14/12/12.
+//  Copyright (c) 2014年 weichen. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
